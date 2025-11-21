77afd3a360541f737475e71e4c333ffb3683d431
Buzurgmehr06 <99078444+Buzurgmehr06@users.noreply.github.com>
2025-11-21 20:00:53 +0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
-
+import pandas as pd
 st.title('DS_2025 First Project')
 
 st.write('Good job!')
 
 with st.expander("**Data**"):
   st.write("data ai")
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
