19c5cf8643bf1c4e048048c68ef43102fa5c4658
Buzurgmehr06 <99078444+Buzurgmehr06@users.noreply.github.com>
2025-11-24 18:59:36 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,30 @@
 import streamlit as st
 import pandas as pd
-st.title('DS_2025 First Project')
 
-st.write('Good job!')
+st.title('🤖 MY FIRST PROJECT')
 
-with st.expander("**Data**"):
-  st.write("data ai")
+st.write('Lets do it!')
+
+with st.expander("DATA"):
+  st.write('**OUR DATA**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
-with st.expander("Plot"):
-  st.scatter_chart(data = df,x = 'bill_length_mm', y = 'bill_depth_mm', color = 'species')
+
+with st.expander("Visualization"):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+X_raw = df.drop('species', axis=1)
+y_raw = df.species
+
 with st.sidebar:
   st.header('Input features')
-  island = st.selectbox('Island', df['island'].unique())
+  island = st.selectbox('**Island**', ('Torgersen', 'Dream', 'Biscoe'))
   bill_length_mm = st.slider("**bill_length_mm**", 32.1, 59.6, 34.1)
   bill_depth_mm = st.slider("**bill_depth_mm**", 32.1, 59.6, 45.1)
   flipper_length_mm = st.slider("**flipper_length_mm**", 32.1, 59.6, 55.1)
   body_mass_g = st.slider("**body_mass_g**", 2700, 6300, 4207)
-  sex = st. selectbox("**Gender**", ('male', 'female'))
+  sex = st.selectbox('**Gender**', ('male', 'female'))
+
 data = {'island':island,
         'bill_length_mm':bill_length_mm,
         'bill_depth_mm':bill_depth_mm,
