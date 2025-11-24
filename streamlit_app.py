b20205967fe55e6199bbc323d9b3207b145c84fc
Buzurgmehr06 <99078444+Buzurgmehr06@users.noreply.github.com>
2025-11-24 18:56:16 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,11 @@ with st.sidebar:
   flipper_length_mm = st.slider("**flipper_length_mm**", 32.1, 59.6, 55.1)
   body_mass_g = st.slider("**body_mass_g**", 2700, 6300, 4207)
   sex = st. selectbox("**Gender**", ('male', 'female'))
+data = ('island': island,
+        'bill _length_mm' :bill_length_mm,
+        'bill depth_mm':bill_depth_mm,
+        'flipper_length_mm':flipper_length_mm,
+        'body_mass_g': body_mass_g,
+        'sex': sex)
+input _df = pd.DataFrame(data, index=[0])
+input _penguins = pd.concat ([input_df, X_raw], axis=0)
